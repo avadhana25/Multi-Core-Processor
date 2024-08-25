d1bc09e08d0341d2ffd8866f5573de934c0066f6
Aakash Vadhanam <avadhana@purdue.edu>
2024-08-25 11:56:42 -0400

    Testbench complete

---
+++ source/register_file.sv
@@ -0,0 +1,76 @@
+/*
+Aakash Vadhanam
+register file source file
+8/21/24
+*/
+
+//all types
+`include "cpu_types_pkg.vh"
+
+// import types
+  import cpu_types_pkg::*;
+
+
+module register_file(input logic CLK, nRST, register_file_if.rf rfif);
+
+word_t [31:0] register, new_register;
+
+//flip flop for all of the registers
+always_ff@(posedge CLK or negedge nRST)
+begin
+    if (nRST == 0)
+    begin
+        register     <= '0;
+    end
+    else
+    begin
+        register[0] = '0;              //register 0 is always all 0s
+        for (int i = 1; i < 32; i++) 
+        begin
+            register[i] = new_register[i];
+        end
+    end
+end
+
+//Combination logic for Write Command
+always_comb
+begin
+    //defeault values
+    new_register = register;
+
+    //logic
+    if (rfif.WEN)
+    begin
+        new_register[rfif.wsel] = rfif.wdat;        
+    end    
+end
+
+//Combination logic for Read 1 Command
+always_comb
+begin
+    //default values
+    rfif.rdat1 = '0;
+
+    //logic
+    if (rfif.rsel1)
+    begin
+        rfif.rdat1 = register[rfif.rsel1];
+    end
+end
+
+//Combination logic for Read 2 Command
+always_comb
+begin
+    //default values
+    rfif.rdat2 = '0;
+
+    //logic
+    if (rfif.rsel2)
+    begin
+        rfif.rdat2 = register[rfif.rsel2];
+    end
+end
+
+
+
+endmodule
--- testbench/register_file_tb.sv
+++ testbench/register_file_tb.sv
@@ -5,6 +5,7 @@
   register file test bench
 */
 
+
 // mapped needs this
 `include "register_file_if.vh"
 
@@ -16,6 +17,7 @@ module register_file_tb;
   parameter PERIOD = 10;
 
   logic CLK = 0, nRST;
+  logic [31:0] [31:0] register;
 
   // test vars
   int v1 = 1;
@@ -28,7 +30,7 @@ module register_file_tb;
   // interface
   register_file_if rfif ();
   // test program
-  test PROG ();
+  test #(.PERIOD (PERIOD)) PROG ();
   // DUT
 `ifndef MAPPED
   register_file DUT(CLK, nRST, rfif);
@@ -46,7 +48,130 @@ module register_file_tb;
   );
 `endif
 
+//tasks
+
+task reset_dut;
+begin
+
+  nRST = 1'b0;
+  @(posedge CLK);
+  @(posedge CLK);
+  @(negedge CLK);
+  nRST = 1'b1;
+  @(posedge CLK);
+  @(posedge CLK);
+end
+endtask
+/*
+task print_register;
+input [31:0] curr_register;
+input reg_count;
+begin
+    $monitor("@%00g CLK = %b nRST = %b register[%d] = %h",
+    $time, CLK, nRST, reg_count, curr_register);
+  
+end
+
+  
+endtask //automatic
+*/
 endmodule
 
 program test;
+  parameter PERIOD = 10;
+  integer testcase;
+  string testdesc;
+  initial begin
+    //initialize inputs
+    rfif.wdat = '0;
+    rfif.wsel = '0;
+    rfif.rsel1 = '0;
+    rfif.rsel2 = '0;
+    rfif.WEN = 0;
+
+    //TESTCASE 1: ASYNC RESET
+    testcase = 1;
+    testdesc = "Asynchronous Reset";
+
+    reset_dut;
+
+    $display("TESTCASE %0d: %s", testcase, testdesc);
+    for (int i = 0; i < 32; i++) 
+    begin
+      #(PERIOD)
+      #(PERIOD)
+      if ((rfif.rdat1 == '0) && (rfif.rdat2 == '0))
+      begin
+        $display("Register %0d successefully matches", i);
+      end
+      else
+      begin
+        $display("Register %0d DOESN'T match", i);
+      end
+      rfif.rsel1 = rfif.rsel1 + 1;
+      rfif.rsel2 = rfif.rsel2 + 1;
+    end
+
+
+    //TESTCASE 2: Write to Register 0
+    testcase = 2;
+    testdesc = "Write to Register Zero";
+
+    rfif.rsel1 = 0;
+    rfif.rsel2 = 0;
+    
+    reset_dut;
+
+    rfif.WEN = 1;
+    rfif.wsel = 0;
+    rfif.wdat = $random;
+
+    $display("TESTCASE %0d: %s", testcase, testdesc);
+    #(PERIOD)
+    #(PERIOD)
+    if ((rfif.rdat1 == '0) && (rfif.rdat2 == '0))
+    begin
+      $display("Register 0 successefully matches");
+    end
+    else
+    begin
+      $error("Register 0 DOESN'T match");
+    end
+
+
+    //TESTCASE 3: Writes and Reads to all other registers
+    testcase = 3;
+    testdesc = "Writes and Reads to All Other Registers";
+
+    reset_dut;
+
+    rfif.WEN = 1;
+    rfif.wsel = 5'b1;
+    rfif.rsel1 = 5'b1;
+    rfif.rsel2 = 5'b1;
+
+    $display("TESTCASE %0d: %s", testcase, testdesc);
+    for (int i = 1; i < 32; i++) 
+    begin
+      rfif.wdat = $random;
+      #(PERIOD)
+      #(PERIOD)
+      if ((rfif.rdat1 == rfif.wdat) && (rfif.rdat2 == rfif.wdat))
+      begin
+        $display("Register %0d successefully matches random wdat", i);
+      end
+      else
+      begin
+        $display("Register %0d DOESN'T match", i);
+        $display("Register %0d: %b vs %b", i, rfif.rdat1, rfif.wdat);
+      end
+      rfif.rsel1 = rfif.rsel1 + 1;
+      rfif.rsel2 = rfif.rsel2 + 1;
+      rfif.wsel  = rfif.wsel + 1;
+    end
+
+    $finish;
+      
+   
+  end
 endprogram
