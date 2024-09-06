f046056d542a65ac8c427a16c1bf84aa85b0a6d4
Aakash Vadhanam <avadhana@purdue.edu>
2024-09-06 18:59:40 -0400

    about to change by alot

--- source/control_unit.sv
+++ source/control_unit.sv
@@ -29,12 +29,13 @@ begin
   cuif.rs1    = regbits_t'(cuif.instr[19:15]);
   cuif.rs2    = regbits_t'(cuif.instr[24:20]);
   cuif.rd     = regbits_t'(cuif.instr[11:7]);
-  func3       = funct3_i_t'(cuif.instr[14:12]);
+  func3       = funct3_r_t'(cuif.instr[14:12]);
   func7       = cuif.instr[31:25];
   cuif.imm    = cuif.instr[31:20];
 
 
   //control logic signals
+  cuif.iREN   = 1'b1;
   cuif.dWEN   = 1'b0;
   cuif.dREN   = 1'b0;
   cuif.regWr  = 1'b0;
@@ -43,6 +44,9 @@ begin
   cuif.rdSel  = 3'b0;         //0: alu_out  1: d_memload  2:JAL or JALR  3: LUI  4:AUIPC
   cuif.shift  = 1'b0;
   cuif.jpSel  = 1'b0;
+  cuif.halt   = 1'b0;
+  cuif.aluOp  = ALU_ADD;
+
 
 
 
@@ -124,12 +128,14 @@ begin
 
   ITYPE:        //excludes LW and JALR
   begin
+    //instruction fields
+    func3 = funct3_i_t'(cuif.instr[14:12]);
+
     //control signals
     cuif.regWr  = 1'b1;         //write to register
     cuif.aluSrc = 1'b1;         //use sign extended imm as second alu input
     cuif.pcSrc  = 2'b0;         //PC = PC + 4
     cuif.rdSel  = 3'b0;         // take alu_output line
-    cuif.shift  = 1'b0;         //use full imm value
     
     //func-specific control signals
     casez (func3)
@@ -192,8 +198,8 @@ begin
     //control signals
     cuif.regWr  = 1'b1;      //write to register
     cuif.aluSrc = 1'b1;      //use imm as second alu input
-    cuif.shift  = 1'b0;      //use full imm value
     cuif.pcSrc  = 2'b0;      //PC = PC + 4
+    cuif.dREN   = 1'b1;      //enable load from memory
     cuif.rdSel  = 3'b1;      //take memory loaded from memory
     cuif.aluOp  = ALU_ADD;   //always add for load
   end
@@ -203,26 +209,112 @@ begin
     //control signals
     cuif.regWr  = 1'b1;      //write to register
     cuif.aluSrc = 1'b1;      //use imm as second alu input
-    cuif.shift  = 1'b0;      //use full imm value
     cuif.rdSel  = 3'h2;      //load npc into rd
     cuif.pcSrc  = 2'h2;      //PC = alu output
     cuif.aluOp  = ALU_ADD;   //always add for jalr
   end
 
+  STYPE:           //SW
+  begin
+    //adjust imm field
+    cuif.imm    = {cuif.instr[31:25], cuif.instr[11:7]};
+
+    //control signals
+    cuif.regWr  = 1'b0;      //do not write to register
+    cuif.aluSrc = 1'b1;      //use imm as second alu input
+    //cuif.rdSel = x         //dont care value do to RegWr being 0
+    cuif.dWEN   = 1'b1;      //write value to memory
+    cuif.pcSrc  = 2'b0;      //PC = PC+4
+    cuif.aluOp  = ALU_ADD;   //always add for sw
+  end
+
+  BTYPE:        //Branches
+  begin
+    //instruction fields
+    func3 = funct3_b_t'(func3);
+
+    //control signals
+    cuif.regWr  = 1'b0;      //do not write to register
+    cuif.aluSrc = 1'b0;      //use both register values
+    //cuif.rdSel = x         //dont care value due to regWr being 0
+    cuif.jpSel = 1'b0;     //use Branch Address
+
+    if (cuif.branch)               //if branch taken
+    begin
+        cuif.pcSrc = 2'b1;     //PC = branchaddr
+    end
+    else
+    begin
+        cuif.pcSrc = 2'b0;     //else pc = pc + 4
+    end
+
+    casez (func3) 
 
 
-    
+    BEQ:
+    begin
+        cuif.aluOp = ALU_SUB;
+    end
+
+    BNE:
+    begin
+        cuif.aluOp = ALU_SUB;
+    end
+
+    BLT:
+    begin
+        cuif.aluOp = ALU_SLT;
+    end
+
+    BGE:
+    begin
+        cuif.aluOp = ALU_SLT;
+    end
+
+    BLTU:
+    begin
+        cuif.aluOp = ALU_SLTU;
+    end
+
+    BGEU:
+    begin
+        cuif.aluOp = ALU_SLTU;
+    end
 
 
+    endcase
+  end
 
+  JAL:      //Jtype
+  begin
+    //control signals
+    cuif.regWr   = 1'b1;               //write to register
+    cuif.rdSel   = 3'h2;               //write npc to register
+    cuif.jpSel = 1'b1;               //take jumpaddr
+    cuif.pcSrc   = 2'b1;               //pc = pc + jumpaddr  
+  end
 
+  LUI:      //Utype
+  begin
+    //control signals
+    cuif.regWr  = 1'b1;           //write to register
+    cuif.rdSel  = 3'h3;           //write {imm,12'b0} to register
+    cuif.pcSrc  = 2'b0;           //pc = pc +4;
+  end
 
+  AUIPC:      //Utype
+  begin
+    //control signals
+    cuif.regWr  = 1'b1;           //write to register
+    cuif.rdSel  = 3'h4;           //write pc + {imm,12'b0} to register
+    cuif.pcSrc  = 2'b0;           //pc = pc +4;
+  end
 
-
-    
-
-
-
+  HALT:
+  begin
+    cuif.halt = 1'b1;
+    cuif.iREN = 1'b0;
+  end
 
   endcase
 
