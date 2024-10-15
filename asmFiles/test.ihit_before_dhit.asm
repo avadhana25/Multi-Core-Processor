af8fe2772cf4f300b376ea5f49cfcd6dbd26517b
Brett Heckman <bheckman@purdue.edu>
2024-10-15 17:24:22 -0400

    new asm files

--- asmFiles/test.ihit_before_dhit.asm
+++ asmFiles/test.ihit_before_dhit.asm
@@ -2,8 +2,9 @@
 org 0x0000
     ori   $4, $0, 0xF0
     addi  $5, $0, 2
-    loop: lw $6, 0($4)
-           addi $7,$0,1 //lw in decode
+    loop:  or $4,$4,$7
+           lw $6, 0($4)
+           addi $7,$0,8 //lw in decode
            addi $8,$0,1 //lw in execute
            addi $9,$0,1 //lw in mem, should be ihit and dhit in second iteration
            addi $5,$5,-1
@@ -14,3 +15,6 @@ org 0x0000
 org   0x00F0
   cfw   0x7337
   cfw   0x2701
+
+org  0x00F8
+  cfw  0x1234
