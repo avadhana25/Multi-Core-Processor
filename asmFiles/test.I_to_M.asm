f54c83002b3e67ed9b57f336241bbcdb0cd96f99
Aakash Vadhanam <avadhana@purdue.edu>
2024-10-25 17:35:05 -0400

    fixed unit tests for transition

--- asmFiles/test.I_to_M.asm
+++ asmFiles/test.I_to_M.asm
@@ -2,17 +2,25 @@
 # RISC-V Assembly
 #----------------------------------------------------------
 #--------------------------------------
-# Test Block transition from Invalid to Modified
+# Test Block transition from Invalid to Modified and Modified to Invalid
 #--------------------------------------
 org 0x0000
-  addi $4, $0, 0x00F0     #memory
+  addi $4, $0, 0xF000     #memory
   addi $5 , $0, 0x6226
-  sw $5, 0($4)           #block at address 0x00F0 should now be in modified
+  sw $5, 0($4)           #block at address 0xF000 should now be in modified
+  nop
   halt
 
 org 0x0200
+  addi $4, $0, 0xF000
+  addi $5, $0, 0x8448
+  nop
+  sw $5, 8($4)           #block at address 0xF000 goes to Invalid, block at 0xF008 goes to Modified from Invalid
+
     halt
 
 org   0xF000
   cfw   0x7337
   cfw   0x2701
+  cfw   0x6116
+  cfw   0x1611
--- asmFiles/test.M_to_M.asm
+++ asmFiles/test.M_to_M.asm
@@ -5,7 +5,7 @@
 # Test Block transition from Modified to Modified
 #--------------------------------------
 org 0x0000
-  addi $4, $0, 0x00F0     #memory
+  addi $4, $0, 0xF000     #memory
   sw $5, 0($4)           #block at address 0x00F0 should now be in modified
   addi $6, $0, 0x0011
   sw $6, 4($4)           #block should stay in Modified
