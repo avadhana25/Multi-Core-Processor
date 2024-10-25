86e26353c6e540e8c84b199efff4d03c7a050685
ECE 437L <ece437l@ecn.purdue.edu>
2024-10-25 19:14:08 -0400

    fixed example.asm

--- asmFiles/example.asm
+++ asmFiles/example.asm
@@ -4,80 +4,94 @@
 #------------------------------------------
 # Originally Test and Set example by Eric Villasenor
 # Modified to be LL and SC example by Yue Du
+# Modified for RISC-V by Zach Lagpacan
 #------------------------------------------
 
 #----------------------------------------------------------
-# First Processor
+# Core 1 Init
 #----------------------------------------------------------
-  org   0x0000    
-            # first processor p0
-  lui $3, 0xffffc
-  ori   $2, $0, 0xffc  # stack
-  sub $2, $2, $3
-  addi $31, $0, 1
-   jal   mainp0              # go to program
+  org 0x0000    
+  li      sp, 0xFFFC    # core 1 stack
+  jal     mainc1        # core 1 main program
   halt
 
+#----------------------------------------------------------
+# Shared Lock Functions
+#----------------------------------------------------------
 # pass in an address to lock function in argument register 0
 # returns when lock is available
 lock:
-aquire:
-  lr.w    $t0, ($a0)         # load lock location
-   bne   $t0, $31, aquire     # wait on lock to be open
-   addi $t0, $0, 0
-   sc.w  $t3, $t0, ($a0)
-   beq   $t3, $31, lock       # if sc.w failed retry (In case of SC failuer, rs gets written 1)
-   jr    $ra
- 
+acquire:
+  lr.w    t0, (a0)              # load lock location
+  bne     t0, zero, acquire     # wait on lock to be open
+  li      t1, 1
+  sc.w    t2, t1, (a0)
+  bne     t2, zero, lock        # if sc.w failed, retry (In case of SC failure, rd gets written 1 (!= 0))
+  ret
 
 # pass in an address to unlock function in argument register 0
-# returns when lock is free
+# returns after freeing lock
 unlock:
-  sw    $31, 0($a0)
-   jr    $ra
- 
+  sw      zero, 0(a0)           # exclusive writer safe to clear the lock
+  ret
+
+#----------------------------------------------------------
+# Core 1 Main
+#----------------------------------------------------------
 # main function does something ugly but demonstrates beautifully
-mainp0:
-  push  $ra                 # save return address
-   ori   $a0, $0, l1      # move lock to arguement register
-   jal   lock                # try to aquire the lock
-  # critical code segment
-  ori   $t2, $0, res
-   lw    $t0, 0($t2)
-   addi $t1, $t0, 2
-   sw    $t1, 0($t2)
-   # critical code segment
-  ori   $a0, $0, l1      # move lock to arguement register
-   jal   unlock              # release the lock
-  pop  $ra                 # get return address
-   jr   $ra                 # return to caller
- l1:
-  cfw 0x1
+mainc1:
+  push    ra                    # save return address to main
+  ori     a0, zero, lock_var    # move lock to argument register
+  jal     lock                  # try to acquire the lock
 
+  # ----------------------- #
+  # critical code segment:
+  ori     t2, zero, res
+  lw      t0, 0(t2)
+  addi    t1, t0, 2
+  sw      t1, 0(t2)
+  # ----------------------- #
+
+  ori     a0, zero, lock_var    # move lock to argument register
+  jal     unlock                # release the lock
+  pop     ra                    # get return address
+  ret
 
 #----------------------------------------------------------
-# Second Processor
+# Core 2 Init
 #----------------------------------------------------------
-  org   0x200               # second processor p1
-  ori   $sp, $zero, 0x7ffc  # stack
-   jal   mainp1              # go to program
+  org 0x0200               
+  li      sp, 0x7FFC            # core 2 stack
+  jal     mainc2                # core 2 main program
   halt
 
+#----------------------------------------------------------
+# Core 2 Main
+#----------------------------------------------------------
 # main function does something ugly but demonstrates beautifully
-mainp1:
-  push  $ra                 # save return address
-   ori   $a0, $zero, l1      # move lock to arguement register
-   jal   lock                # try to aquire the lock
-  # critical code segment
-  ori   $t2, $zero, res
-   lw    $t0, 0($t2)
-   addi $t1, $t0, 1
-   sw    $t1, 0($t2)
-   # critical code segment
-  ori   $a0, $zero, l1      # move lock to arguement register
-   jal   unlock              # release the lock
-  pop   $ra                 # get return address
-   jr    $ra                 # return to caller
- 
+mainc2:
+  push    ra                    # save return address
+  ori     a0, zero, lock_var    # move lock to argument register
+  jal     lock                  # try to acquire the lock
+
+  # ----------------------- #
+  # critical code segment:
+  ori     t2, zero, res
+  lw      t0, 0(t2)
+  addi    t1, t0, 1
+  sw      t1, 0(t2)
+  # ----------------------- #
+
+  ori   a0, zero, lock_var    # move lock to argument register
+  jal   unlock                # release the lock
+  pop   ra                    # get return address
+  ret
+
+#----------------------------------------------------------
+# Shared Data Segment
+#----------------------------------------------------------
+org 0x0300
+lock_var:
+  cfw 0x0     # lock starts unlocked, should end unlocked
 res:
-  cfw 0x0                   # end result should be 3
+  cfw 0x0     # end result should be 3
