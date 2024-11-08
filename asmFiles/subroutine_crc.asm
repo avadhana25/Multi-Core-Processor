70caf69a35353a6cb9838fdb2f410d63de264f71
ECE 437L <ece437l@ecn.purdue.edu>
2024-11-08 12:26:44 -0500

    Updated subroutine_crc

--- asmFiles/subroutine_crc.asm
+++ asmFiles/subroutine_crc.asm
@@ -2,42 +2,42 @@
 # RISC-V Assembly
 #----------------------------------------------------------
 #REGISTERS
-#at $1 at
-#v $2-3 function returns
-#a $4-7 function args
-#t $8-15 temps
-#s $16-23 saved temps (callee preserved)
-#t $24-25 temps
-#k $26-27 kernel
-#gp $28 gp (callee preserved)
-#sp $29 sp (callee preserved)
-#fp $30 fp (callee preserved)
-#ra $31 return address
+#ra $1 Return address
+#sp $2 Stack pointer
+#gp $3 Global pointer
+#tp $4 Thread pointer
+#t0-2 $5-7 temps
+#s0/fp $8 Saved/frame pointer
+#s1 $9 Saved register
+#$a0-1 $10-11 Fn args/return values
+#$a2-7 $12-17 Fn args
+#$s2-11 $18-27 Saved registers
+#$t3-6 $28-31 Temporaries
 
 # USAGE random0 = crc(seed), random1 = crc(random0)
 #       randomN = crc(randomN-1)
 #------------------------------------------------------
-# $v0 = crc32($a0)
+# $a0 = crc32($a2)
 crc32:
-  lui $6, 0x04C11
-   ori $6, $6, 0x7B7
-   addi $6, $6, 0x600
-   or $7, $0, $0
-   ori $28, $0, 32
+  lui $t1, 0x04C11
+   ori $t1, $t1, 0x7B7
+   addi $t1, $t1, 0x600
+   or $t2, $0, $0
+   ori $t3, $0, 32
  
 l1:
-  slt $29, $7, $28
-   beq $29, $0, l2
+  slt $t4, $t2, $t3
+   beq $t4, $0, l2
  
-  ori $30, $0, 31
-   srl $29,$12,$30
-   ori $30, $0, 1
-   sll $12,$12,$30
-   beq $29, $0, l3
-   xor $12, $12, $6
+  ori $t5, $0, 31
+   srl $t4, $a2, $t5
+   ori $t5, $0, 1
+   sll $a2,$a2,$t5
+   beq $t4, $0, l3
+   xor $a2, $a2, $t1
  l3:
-  addi $7, $7, 1
+  addi $t2, $t2, 1
    j l1
 l2:
-  or $10, $12, $0
+  or $a0, $a2, $0
    jr $1
